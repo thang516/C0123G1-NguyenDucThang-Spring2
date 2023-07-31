e11cb4cd96278fa3be1e2d07250b5721a9148cac
thang516 <nguyenthangfa2001@gmail.com>
2023-07-31 08:10:03 +0700

    bt

---
+++ prototype/main.js
@@ -0,0 +1,23 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    const mobileNavShow = document.querySelector('.mobile-nav-show');
+    const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+    document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+        el.addEventListener('click', function(event) {
+            event.preventDefault();
+            mobileNavToogle();
+        })
+    });
+
+    function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavShow.classList.toggle('d-none');
+        mobileNavHide.classList.toggle('d-none');
+    }
+
+
+
+});
